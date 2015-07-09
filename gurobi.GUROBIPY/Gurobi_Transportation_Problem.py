cc597798246816f79a66a3409fadfe23d7201c19
James Gaboardi <jgaboardi@fsu.edu>
2015-07-09 15:49:12 -0400

    update
    update

--- gurobi.GUROBIPY/Gurobi_Transportation_Problem.py
+++ gurobi.GUROBIPY/Gurobi_Transportation_Problem.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 GNU LESSER GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
@@ -7,26 +8,39 @@ GNU LESSER GENERAL PUBLIC LICENSE
 '''
 # Building and Optimizing The Transportation Problem in
 #        Python/Gurobi[gurobipy]
-# Change dimensions of Cij and the Facility Constraint for varying
-#        spatial extents
+# Supply MUST equal Demand
 
 import numpy as np
 import gurobipy as gbp
 import time
 t1 = time.time()
 
-#     1. Read In Data
+#           1. Read In (or Create) Data
+'''
+# Cost Matrix
 Cij = np.array([4,5,4,10,6,3,3,5,8])
 Cij = Cij.reshape(3,3)
-rows, cols = Cij.shape
+# Supply
 Si = np.array([100,130,140])
 Si = Si.reshape(3,1)
+# Demand
 Dj = np.array([150,100,120])
-#Dj = Dj.reshape(1,3)
-client_nodes = range(len(Cij))
+'''
+# Cost Matrix
+# 500x500
+Cij = np.random.randint(100, 10000, 250000)
+Cij = Cij.reshape(500,500)
+# Supply
+Si = np.random.randint(50, 200, 500)
+Si = Si.reshape(500,1)
+SiSum = np.sum(Si)
+# Demand
+Dj = np.sort(Si, axis=None)
+DjSum = np.sum(Dj)
+            client_nodes = range(len(Cij))
 
 #       2. Create Model, Set MIP Focus, Add Variables, & Update Model
-m = gbp.Model(' -- TSP -- ')
+m = gbp.Model(' -- The Transportation Problem -- ')
 # Set MIP Focus to 2 for optimality
 gbp.setParam('MIPFocus', 2)
 client_var = []
@@ -58,14 +72,24 @@ for orig in client_nodes:
 
 #       5. Optimize and Print Results
 m.optimize()
-print 'Selected Facility Locations:'
+t2 = time.time()-t1
+print '*****************************************************************************************'
+print '    | From SUPPLY Facility to DEMAND Facility x(Si)_(Dj) shipping # of units  '
+print '    |                                         ↓↓        ↓↓     ↓↓'
 selected = []
 for v in m.getVars():
     if v.x > 0:
+        var = '%s' % v.VarName
+        value = '%i' % v.x
         selected.append(v.x)
-        print('            Variable %s = %g' % (v.varName, v.x))
-print 'Candidate Facilities          *', len(selected)
-print('Rounded Objective (min):      * %g' % m.objVal)
-print 'Real Time to Optimize (sec.): *', time.time()-t1
-print '\n-----\nJames Gaboardi, 2015'
-m.write('/Users/jgaboardi/Desktop/TSP.lp')
+        print '    |                                       ', var, '____ Units: ' ,value
+print '    | Selected Facility Locations ---------  ^^^^^^^^^^^^^^^^^^^^^^^ '
+print '    | Candidate Facilities [p] ------------ ', len(selected)
+print '    | Supply Sum -------------------------- ', SiSum
+print '    | Demand Sum -------------------------- ', DjSum
+val = m.objVal
+print '    | Objective Value --------------------- ', int(val)
+print '    | Real Time to Optimize (sec.) -------- ', t2
+print '*****************************************************************************************'
+print '\nJames Gaboardi, 2015'
+m.write('path.lp')
