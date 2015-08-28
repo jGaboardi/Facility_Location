c056c28369b26648f0f040015235f320ba3002b8
James Gaboardi <jgaboardi@fsu.edu>
2015-08-28 15:20:34 -0400

    update
    update

---
+++ LP_File_Creation/PDefense_Sum_CODE.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#The p-Defense-Sum Facility Location Problem
+#This script creates a linear programming file to be read into an optimizer.
+'''
+GNU LESSER GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+'''
+# Developed by:  James D. Gaboardi, MSGIS
+#                08/2015
+#                © James Gaboardi
