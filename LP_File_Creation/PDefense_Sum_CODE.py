f3f5a147da170bf0eb01c5265f02467581e8bcb1
James Gaboardi <jgaboardi@fsu.edu>
2015-08-31 00:47:12 -0400

    update
    update

--- LP_File_Creation/PDefense_Sum_CODE.py
+++ LP_File_Creation/PDefense_Sum_CODE.py
@@ -15,4 +15,70 @@ GNU LESSER GENERAL PUBLIC LICENSE
 
 # Maximizing the total distance bewteen facilities (generally noxious)
 
+#    1. IMPORTS
+# Other imports may be necessary for matrix creation and manipulation 
+import numpy as np
 
+
+#    2. DEFINED FUNCTIONS
+
+# Facility Constraint
+# *** '= 1\n' indicates 1 facility  
+def get_p_facilities():
+    outtext = ''
+    for i in range(1, cols+1):
+        temp = ''
+        temp += 'y' + str(i)
+        outtext += temp + ' + '
+    outtext = ' c1:  ' + outtext[:-2] + '= 2\n'
+    return outtext
+
+# Declaration of Bounds
+def get_bounds_facility():
+    outtext = ''
+    for i in range(cols):
+        outtext += ' 0 <= y' + str(i+1) + ' <= 1\n'
+    return outtext
+
+# Declaration of Decision Variable (form can be: Binary, Integer, etc.)
+# In this case decision variables are binary.
+# *** 0 for no sited facility, 1 for a sited facilty
+def get_facility_decision_variables():  
+    outtext = ''
+    for i in range (1, cols+1):
+        outtext += 'y' + str(i) + ' '
+    #outtext += temp
+    return outtext 
+    
+#    3. DATA READS & VARIABLE DECLARATION
+Cij = np.random.randint(1,20,16)
+Cij = Cij.reshape(4,4)
+rows,cols = Cij.shape
+
+
+#    4. START TEXT FOR .lp FILE
+# Declaration of Objective Function
+text = "p-Defense-Sum Facility Location Problem\n"
+text += "'''\n"
+text += 'Maximize\n'
+
+# Declaration of Constraints
+text += 'Subject To\n'                    
+text += get_p_facilities()
+# Declaration of Bounds
+text += 'Bounds\n'
+text += get_bounds_facility()
+# Declaration of Decision Variables form: Binaries
+text += 'Binaries\n'
+text += get_facility_decision_variables()
+text += '\n'
+text += 'End\n'
+text += "'''\n"
+text += "© James Gaboardi, 2015"
+                
+
+#   5. CREATE & WRITE .lp FILE TO DISK
+# Fill path name  --  File name must not have spaces.
+outfile = open('LP.lp', 'w')
+outfile.write(text)
+outfile.close()
