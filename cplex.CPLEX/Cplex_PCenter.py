10e7f689629f6d382bcaee90c06e9e7b9f3d2dc6
James Gaboardi <jgaboardi@fsu.edu>
2016-05-07 14:10:16 -0400

    commit
    commit

--- cplex.CPLEX/Cplex_PCenter.py
+++ cplex.CPLEX/Cplex_PCenter.py
@@ -1,150 +1,120 @@
-'''
-GNU LESSER GENERAL PUBLIC LICENSE
-                       Version 3, 29 June 2007
- Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
- Everyone is permitted to copy and distribute verbatim copies
- of this license document, but changing it is not allowed.
-'''
+#!/usr/bin/env python
 
-# Building and Optimizing a p-Center facility location problem in
-#        Python/cplex.CPLEX
-
-import numpy as np
 import cplex as cp
+import numpy as np
 import time
+#import pulp
 t1 = time.time()
 
-#     1. Read In Data
-# Cost Matrix
-#Cij = np.fromfile('/path/Cij.txt', dtype=float, sep='\n')
-#Cij = Cij.reshape(4,4)
-Cij = [[  0.,  13.,   8.,  15.],
- [ 13.,   0.,  12.,  11.],
- [  8.,  12.,   0.,  10.],
- [ 15.,  11.,  10.,   0.]]
-# Indices & Variable Names
-nodes = len(Cij)
-Nodes = range(len(Cij))
-all_nodes = len(Cij) * len(Cij)
-ALL_nodes = range(all_nodes)
-W = 'W'
-x = 'x'
-cli_var = []
-for i in Nodes:
-    for j in Nodes:
-        temp = x + str(i+1) + '_' + str(j+1)
-        cli_var.append(temp)
-client_var = np.array(cli_var)
-client_var = client_var.reshape(4,4)
-#print 'Client Variables\n' + str(cli_var)
-y = 'y'
-fac_var = []
-for i in Nodes:
-    temp = y + str(i+1)
-    fac_var.append(temp)
-facility_var = np.array(fac_var)
-facility_var = facility_var.reshape(4,1)
-#print 'Facility Variables\n' + str(fac_var)
+#P_Center = pulp.LpProblem("P-Center Problem", pulp.LpMinimize)
 
-#     2. Create Model and Add Variables
-# Create Model
-m = cp.Cplex()
-# Problem Name
-m.set_problem_name('\n -- P-Center -- ')
-print m.get_problem_name()
-# Problem Type  ==>  Linear Programming
-m.set_problem_type(m.problem_type.LP)
-# Set MIP Emphasis to '2' --> Optimal
-m.parameters.emphasis.mip.set(2)
-print m.parameters.get_changed()
-print '\nProblem Type\n    ' + str(m.problem_type[m.get_problem_type()])
-# Objective Function Sense  ==>  Minimize
-m.objective.set_sense(m.objective.sense.minimize)
-print 'Objective Sense\n    ' + str(m.objective.sense[m.objective.get_sense()])
+#m = pulp.cplex.Cplex()
+m = cp.Cplex()                                      # Create model
+m.parameters.emphasis.mip.set(2)                    # Set MIP emphasis to '2' --> Optimal
+m.set_problem_type(m.problem_type.LP)               # Set problem type
+m.objective.set_sense(m.objective.sense.minimize)   # Objective Function Sense  ==>  Minimize
+
+np.random.seed(352)
+
+# Cost Matrix
+Cij = np.random.uniform(1, 10, 9)
+Cij = Cij.reshape(3,3)
+client_nodes = range(len(Cij))
+service_nodes = range(len(Cij[0]))
+all_nodes_len = len(Cij) * len(Cij[0])
+ALL_nodes_range = range(all_nodes_len)
+
+# Variable Indices
+W = 'W'
+
+client_var = []
+for orig in client_nodes:
+        client_var.append([])
+        for dest in service_nodes:
+            client_var[orig].append('x'+str(orig+1)+'_'+str(dest+1))
+
+fac_var = []
+for dest in service_nodes:
+        fac_var.append([])
+        fac_var[dest].append('y' + str(dest+1))
+
+
 # Add Maximized Minimized Average Time Variable
-m.variables.add(names = [W],
+m.variables.add(names = W,
                 obj = [1],
                 lb = [0],
+                ub = [cp.infinity],
                 types = ['C'])
 # Add Client Decision Variables
-m.variables.add(names = [cli_var[i] for i in ALL_nodes], 
-                        lb = [0] * all_nodes, 
-                        ub = [1] * all_nodes, 
-                        types = ['B'] * all_nodes)
+m.variables.add(names = [client_var[i][j] for i in client_nodes for j in service_nodes], 
+                        lb = [0] * all_nodes_len, 
+                        ub = [1] * all_nodes_len, 
+                        types = ['B'] * all_nodes_len)
 # Add Service Decision Variable
-m.variables.add(names = [fac_var[i] for i in Nodes],
-                        lb = [0] * nodes, 
-                        ub = [1] * nodes, 
-                        types = ['B'] * nodes)
+m.variables.add(names = [fac_var[j][0] for j in service_nodes],
+                        lb = [0] * len(Cij[0]), 
+                        ub = [1] * len(Cij[0]), 
+                        types = ['B'] * len(Cij[0]))
+
 
 #    3. Add Constraints
 # Add Assignment Constraints
-for orig in Nodes:       
+for orig in client_nodes:       
     assignment_constraints = cp.SparsePair(ind = [client_var[orig][dest] 
-                                            for dest in Nodes],                           
-                                            val = [1] * nodes)
+                                            for dest in service_nodes],                           
+                                            val = [1] * len(Cij[0]))
     m.linear_constraints.add(lin_expr = [assignment_constraints],                 
                                 senses = ['E'], 
                                 rhs = [1]);
 # Add Facility Constraint
-facility_constraint = cp.SparsePair(ind = fac_var, 
-                                    val = [1.0]*nodes)
+facility_constraint = cp.SparsePair(ind = [fac_var[j][0] for j in service_nodes], 
+                                    val = [1.0] * len(Cij[0]))
 m.linear_constraints.add(lin_expr = [facility_constraint],
                                 senses = ['E'],
                                 rhs = [1])
 # Add Opening Constraint
-cli_var_open = []
-for i in Nodes:
-    for j in Nodes:
-        temp = x + str(j+1) + '_' + str(i+1)
-        cli_var_open.append(temp)
-fac_var_open = []
-for i in Nodes:
-    for j in Nodes:
-        temp = y + str(i+1)
-        fac_var_open.append(temp)
-l = []
-for i in ALL_nodes:
-    l.append([cli_var_open[i]]+[fac_var_open[i]])
-for i in l:
-    opening_constraint = cp.SparsePair(ind = i, val = [-1.0, 1.0])
-    m.linear_constraints.add(lin_expr = [opening_constraint], senses = ['G'], rhs = [0])
-# Add Maximized Minimized Average Time Constraint       
-cli_var_max = []
-for i in Nodes:
-    cli_var_max.append([])
-    for j in Nodes:
-        temp = x + str(i+1) + '_' + str(j+1)
-        cli_var_max[i].append(temp)
-l2 = []
-for orig in Nodes:
-    l2.append([cli_var_max[orig][dest] for dest in Nodes] + [W])
-l3 = []
-for orig in Nodes:
-    l3.append([Cij[orig][dest] for dest in Nodes] + [-1])
-for orig in Nodes:
-    max_constraints = cp.SparsePair(ind = l2[orig], val = l3[orig])
+OC = [[client_var[i][j]] + [fac_var[j][0]] for i in client_nodes for j in service_nodes]
+for oc in OC:
+    #print oc
+    opening_constraints = cp.SparsePair(ind = oc, val = [-1.0, 1.0])
+    m.linear_constraints.add(lin_expr = [opening_constraints], 
+                                senses = ['G'], 
+                                rhs = [0])
+# Add Maximized Minimized Average Time Constraint
+MC = [[],[]]
+for i in client_nodes:
+    MC[0].append([client_var[i][j] for j in service_nodes] + [W])
+    MC[1].append([Cij[i][j] for j in service_nodes] + [-1])
+#print MC
+#val = []
+#for i in client_nodes:
+#    val.append([Cij[i][j] for j in service_nodes] + [-1])
+#print val
+for i in client_nodes:   
+    #max_constraints = cp.SparsePair(ind = MC[i],                           
+    #                                val = val[i])
+    max_constraints = [MC[0][i], MC[1][i]]                            
+    #print max_constraints
     m.linear_constraints.add(lin_expr = [max_constraints],                 
                                 senses = ['L'], 
                                 rhs = [0]);
 
-#    4. Optimize and Print Results
+
+
+m.write('/Users/jgaboardi/Desktop/pathCENTER.lp')
 m.solve()
+t2 = time.time()-t1
 solution = m.solution
-# solution.get_status() returns an integer code
-print 'Solution status = ' , solution.get_status(), ':',
-print solution.status[solution.get_status()]
+
 # Display solution.
-print 'Total cost = ' , solution.get_objective_value()
-print 'Determination Time = ', m.get_dettime(), 'ticks'
-print 'Real Time          = ', time.time()-t1, 'sec.'
 print '****************************'
 for f in fac_var:
-    if (solution.get_values(f) >
-        m.parameters.mip.tolerances.integrality.get()):
-        print '    Facility %s is open' % f
-    else:
-        print '    Facility %s is closed' % f      
+    if solution.get_values(f[0]) > 0 :
+        print 'Facility %s is open' % f[0]
+print 'Solution status    = ' , solution.get_status(), ':', solution.status[solution.get_status()]
+print 'Total cost         = ' , solution.get_objective_value()
+print 'Determination Time = ', m.get_dettime(), 'ticks'
+print 'Real Time          = ', t2/60, 'min.'        
+print 'Matrix Size        = ', Cij.shape
 print '****************************'
-print '\n-----\nJames Gaboardi, 2015'
-m.write('path.lp')
+print '\n-----\nJames Gaboardi, 2016'
