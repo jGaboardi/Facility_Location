d69eb70b5b653bf20463b1fca4bc60d2fb0ffa95
James Gaboardi <jgaboardi@fsu.edu>
2016-05-09 10:44:15 -0400

    commit
    commit

--- cplex.CPLEX/Cplex_SCLP.py
+++ cplex.CPLEX/Cplex_SCLP.py
@@ -17,108 +17,80 @@ np.random.seed(352)
 def Cplex_SetCover(Cij, S):
     t1 = time.time()
     
-    # 
-    Aij = []
+    m = cp.Cplex()                                      # Create model
+    m.set_problem_name('\n -- Set Cover Location Problem -- ')
+    m.parameters.emphasis.mip.set(2)                    # Set MIP emphasis to Optimal
+    m.set_problem_type(m.problem_type.LP)               # Set problem type
+    m.objective.set_sense(m.objective.sense.minimize)   # Objective Function ==>  Minimize
     
+    # Is the value of Cij within S?
+    Aij = []
     for i in Cij:
-        print i
         for j in i:
-            print j
             if j <= S:
                 outtext = 1
             else:
                 outtext = 0
             Aij.append(outtext)
-    print Aij
     rows, cols = Cij.shape
     
     Aij = np.array(Aij)
     Aij = Aij.reshape(rows,cols)
-    
-    client_nodes = range(len(Cij[0]))
 
     # Indices & Variable Names
-    nodes = len(Cij)
-    Nodes = range(len(Cij))
-    all_nodes = len(Cij) * len(Cij)
-    ALL_nodes = range(all_nodes)
+    nodes_length = len(Cij)
+    nodes_range = range(len(Cij))
     
+    # Index for Serialized Variables      
+    matrix_variable = [['x' + str(orig+100001) for orig in nodes_range]] * nodes_length      
     
-    x = 'x'
-    cli_var = []
-    for i in Nodes:
-        for j in Nodes:
-            temp = x + str(j+1)
-            cli_var.append(temp)
-    client_var = np.array(cli_var)
-    client_var = client_var.reshape(4,4)
-    
-    
-    results_var = []
-    for i in Nodes:
-        temp = x + str(i+1)
-        results_var.append(temp)
-
-    #     2. Create Model and Add Variables
-    # Create Model
-    m = cp.Cplex()
-    # Problem Name
-    m.set_problem_name('\n -- Set Cover Location Problem -- ')
-    print m.get_problem_name()
-    # Problem Type  ==>  Linear Programming
-    m.set_problem_type(m.problem_type.LP)
-    # Set MIP Emphasis to '2' --> Optimal
-    m.parameters.emphasis.mip.set(2)
-    print m.parameters.get_changed()
-    print '\nProblem Type\n    ' + str(m.problem_type[m.get_problem_type()])
-    # Objective Function Sense  ==>  Minimize
-    m.objective.set_sense(m.objective.sense.minimize)
-    print 'Objective Sense\n    ' + str(m.objective.sense[m.objective.get_sense()])
     # Add Client Decision Variables
-    m.variables.add(names = [cli_var[i] for i in Nodes],  
-                            obj = [1] * nodes,
-                            lb = [0] * nodes, 
-                            ub = [1] * nodes, 
-                            types = ['B'] * nodes)
+    m.variables.add(names = [matrix_variable[0][i] for i in nodes_range],
+                            obj = [1] * nodes_length,
+                            lb = [0] * nodes_length, 
+                            ub = [1] * nodes_length, 
+                            types = ['B'] * nodes_length)
 
+    
     #    3. Add Constraints 
     #Add Coverage Constraints
-    for orig in Nodes:       
-        coverage_constraints = cp.SparsePair(ind = [client_var[orig][dest] 
-                                                for dest in Nodes],                           
-                                                val = [Aij[orig][dest]for dest in Nodes])
+    for orig in nodes_range:       
+        coverage_constraints = ([matrix_variable[orig][dest] for dest in nodes_range],                           
+                                                [Aij[orig][dest]for dest in nodes_range])
         m.linear_constraints.add(lin_expr = [coverage_constraints],                 
                                     senses = ['G'], 
-                                    rhs = [1]);
+                                    rhs = [1])
 
     #    4. Optimize and Print Results
     m.solve()
     solution = m.solution
     
-    
-    
+
     print 'Total cost = ' , solution.get_objective_value()
     print 'Determination Time = ', m.get_dettime(), 'ticks'
     print 'Real Time to Optimize (sec.): *', time.time()-t1
-    print '****************************'
-    for f in results_var:
+    print '*********************************************'
+    for f in matrix_variable[0]:
         if (solution.get_values(f) >
             m.parameters.mip.tolerances.integrality.get()):
             print '    Facility %s is open' % f
         else:
             print '    Facility %s is closed' % f        
-    print '****************************'
+    print '*********************************************'
     print '\n-----\nJames Gaboardi, 2015'
-    m.write('/path.lp')
+    m.write('path.lp')
     
 ########################################################   
 
  # Cost Matrix
 Sites = 8
 
-Cost_Matrix = np.random.uniform(1, 10, Sites*Sites)
+Cost_Matrix = np.random.uniform(1, 20, Sites*Sites)
 Cost_Matrix = Cost_Matrix.reshape(Sites,Sites)
-print Cost_Matrix[0][0]
-Minimum_Distance = 4
+
+Minimum_Distance =7.
 
 Cplex_SetCover(Cij=Cost_Matrix, S=Minimum_Distance)    
+'''
+James Gaboardi, 2016
+'''
