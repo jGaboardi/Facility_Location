e8103eecfa6e3704afa604a42900d3aea1e6c4e2
James Gaboardi <jgaboardi@fsu.edu>
2016-05-09 11:34:21 -0400

    commit
    commit

--- cplex.CPLEX/Cplex_SCLP.py
+++ cplex.CPLEX/Cplex_SCLP.py
@@ -1,9 +1,20 @@
+
+
 '''
 GNU LESSER GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
  Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
  Everyone is permitted to copy and distribute verbatim copies
  of this license document, but changing it is not allowed.
+'''
+'''
+Originally Published:
+    Toregas, C.
+    ReVelle, Charles.
+    1972.
+    Optimal Location Under Time or Distance Constraints.
+    Papers of the Regional Science Association.
+    28(1):133 - 144. 
 '''
 
 # Building and Optimizing a Set Cover Location Problem problem in 
